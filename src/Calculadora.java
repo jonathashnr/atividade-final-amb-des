3f5034838939e16860500b6c0d1c727eee106963
Jonathas Rodrigues <jonathashnr@gmail.com>
2022-11-29 15:53:52 -0300

    Adiciona classe Calculadora com os métodos comentados

---
+++ src/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+  //public double soma(double parcelaA, double parcelaB) {}
+  //public double subtracao(double minuendo, double subtraendo) {}
+  //public double multiplicacao(double fatorA, double fatorB) {}
+  //public double divisao(double dividendo, double divisor) {}
+  //public double exponencial(double base, double expoente) {}
+  //public double raizQuadrada(double radicando) {}
+  //public double piso(double num) {}
+  //public double teto(double num) {}
+}
