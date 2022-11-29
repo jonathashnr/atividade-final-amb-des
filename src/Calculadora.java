d7e48261e27d2b840628fc2d6863cf9d40b3664a
Eliabe <eliabemonteiro15@gmail.com>
2022-11-29 20:43:01 -0300

    Implementando os métodos de multiplicacao e divisao

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,8 +3,15 @@ import java.lang.Math;
 public class Calculadora {
   //public double soma(double parcelaA, double parcelaB) {}
   //public double subtracao(double minuendo, double subtraendo) {}
-  //public double multiplicacao(double fatorA, double fatorB) {}
-  //public double divisao(double dividendo, double divisor) {}
+
+  public double multiplicacao(double fatorA, double fatorB) {
+    return fatorA*fatorB;
+  }
+
+  public double divisao(double dividendo, double divisor) {
+    return dividendo/divisor; 
+  }
+
   public double exponencial(double base, double expoente) {
     return Math.pow(base, expoente);
   }
