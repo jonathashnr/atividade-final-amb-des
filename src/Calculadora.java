00e1ffbb5e8e6e91ef8c94b8f57883f0e8d0e7c5
Jonathas Rodrigues <jonathashnr@gmail.com>
2022-11-29 16:36:10 -0300

    Adiciona implementação dos métodos exponencial e raizQuadrada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,10 +1,17 @@
+import java.lang.Math;
+
 public class Calculadora {
   //public double soma(double parcelaA, double parcelaB) {}
   //public double subtracao(double minuendo, double subtraendo) {}
   //public double multiplicacao(double fatorA, double fatorB) {}
   //public double divisao(double dividendo, double divisor) {}
-  //public double exponencial(double base, double expoente) {}
-  //public double raizQuadrada(double radicando) {}
+  public double exponencial(double base, double expoente) {
+    return Math.pow(base, expoente);
+  }
+
+  public double raizQuadrada(double radicando) {
+    return Math.sqrt(radicando);
+  }
   //public double piso(double num) {}
   //public double teto(double num) {}
 }
