0f4fb991d3644ed4b6dc1faf39f77f9feaf2c0dd
john22 <eleejhonlima192@hotmail.com.br>
2022-12-01 22:08:20 -0300

    terminada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -25,6 +25,14 @@ public class Calculadora {
   public double raizQuadrada(double radicando) {
     return Math.sqrt(radicando);
   }
+
+  public double piso(double num) {
+      return Math.floor(num);
+  }
+
+  public double teto(double num) {
+      return Math.ceil(num);
+  }
   //public double piso(double num) {}
   //public double teto(double num) {}
 }
