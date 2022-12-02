6ab9d8813809ed2552d7705151d655ac80b2cf52
Jonathas Rodrigues <jonathashnr@gmail.com>
2022-12-01 22:39:18 -0300

    Refatora e corrige classe Soma

--- src/Soma.java
+++ src/Soma.java
@@ -1,9 +1,13 @@
 public class Soma{
-	float $numero0 =0;
-	float $numero1 =0;
-	float $resultado=0;
-	public void somar(){
-		$resultado= $numero0  + $numero1;
-		System.out.println ("Resultado da soma eh de "+ $resultado);
+	float numero1;
+	float numero2;
+	Soma(float numero1, float numero2) {
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	}
+	public void resultado() {
+		Calculadora calc = new Calculadora();
+		double resultado = calc.soma(numero1, numero2);
+		System.out.println ("Resultado da soma eh de "+ resultado);
 	}
 }
